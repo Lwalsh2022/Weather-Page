76276f25e606bf5e97c9f2316e4b9ab70ad0c51b
Lisa Walsh <lwalshphotography@yahoo.com>
2022-10-24 19:57:21 -0400

    updated javascript file

---
+++ script - Copy.js
@@ -0,0 +1,65 @@
+
+$(document).ready;
+const API_KEY = 'a22913bc70f5fe7ee10c9e482a3928e9';
+const iconImg = document.getElementById('weather-icon');
+const loc = document.querySelector('#cityName');
+const tempC = document.querySelector('.c');
+const tempF = document.querySelector('.f');
+const desc = document.querySelector('.desc');
+const tempMax = document.querySelector('#Max')
+const tempMin = document.querySelector('#Min')
+var cityName;
+var base = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + API_KEY;
+ console.log('success')
+
+//  iconImg.src = ("http://openweathermap.org/img/wn/$%7Bicon%7D@2x.png");
+
+ window.addEventListener('load', () => {
+    let long;
+    let lat;
+    // Accessing Geolocation of User
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+       // Storing Longitude and Latitude in variables
+      long = position.coords.longitude;
+      lat = position.coords.latitude; 
+      console.log('success')
+      });
+    }
+  });
+  
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      // Storing Longitude and Latitude in variables
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      const base = `https://api.openweathermap.org/data/2.5/weather?lat=35.4986206&lon=-82.5163212&appid=a22913bc70f5fe7ee10c9e482a3928e9&units=metric`;
+
+      // Using fetch to get data
+      fetch(base)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+       console.log('data', data)
+            const { temp_max } = data.main;
+                const { temp_min } = data.main;
+                const place = data.name;
+                const { description, icon } = data.weather[0];
+        });
+    });
+
+    // loc.textContent = `${place}`;
+    // desc.textContent = `${description}`;
+    // tempC.textContent = `${temp.toFixed(2)} °C`;
+    // tempF.textContent = `${fahrenheit.toFixed(2)} °F`;
+    
+  }
+
+
+
+
+
+
+
+
